d3333903ea21836128f8976b548f62ea5be68007
aman2698 <sikarwaraman26@gmail.com>
2021-08-19 12:56:17 +0530

    Create campaign is completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import {SidebarComponent} from './components/sidebar/sidebar.component'
+// import {SidebarComponent} from './components/sidebar/sidebar.component'
 import {MailerComponent} from './components/mailer/mailer.component'
 import {ClientComponent} from './components/client/client.component'
 import {CampaignComponent} from './components/campaign/campaign.component'
 import {HistoryComponent} from './components/history/history.component'
 import {AuthGuard} from './guard/auth.guard'
 import {PublicGuard} from './guard/public.guard'
+import { CreateCampaignComponent } from './components/create-campaign/create-campaign.component';
 const routes: Routes = [
   {path:'', pathMatch: 'full', redirectTo: '/dashboard' },
   {path:"login",component:LoginComponent , canActivate:[PublicGuard]},
@@ -17,6 +18,7 @@ const routes: Routes = [
   {path:"campaign" ,component:CampaignComponent, canActivate:[AuthGuard]},
   {path:"mailer" ,component:MailerComponent, canActivate:[AuthGuard]},
   {path:"history" ,component:HistoryComponent, canActivate:[AuthGuard]},
+  {path:"create-campaign" ,component:CreateCampaignComponent, canActivate:[AuthGuard]},
   {path:"**" , pathMatch: 'full', redirectTo: '/dashboard'},
 
 ];
